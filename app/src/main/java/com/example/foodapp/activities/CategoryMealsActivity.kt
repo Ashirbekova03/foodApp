ff174621aa31b4fdcdb7df726d476dfabcf21e4f
Ashirbekova03 <ashirbekovaaizhan37@gmail.com>
2024-07-02 11:56:22 +0500

    Initial commit

---
+++ app/src/main/java/com/example/foodapp/activities/CategoryMealsActivity.kt
@@ -0,0 +1,60 @@
+package com.example.foodapp.activities
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.foodapp.R
+import com.example.foodapp.adapters.CategoryMealsAdapter
+import com.example.foodapp.databinding.ActivityCategoryMealsBinding
+import com.example.foodapp.databinding.ActivityMealBinding
+import com.example.foodapp.fragments.HomeFragment
+import com.example.foodapp.videoModel.CategoryMealsViewModel
+import com.example.foodapp.videoModel.MealViewModel
+
+class CategoryMealsActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityCategoryMealsBinding
+    lateinit var categoryMealsViewModel: CategoryMealsViewModel
+    lateinit var categoryMealsAdapter: CategoryMealsAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityCategoryMealsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        prepareRecyclerView()
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //mealMvvm = ViewModelProviders.of(this)[MealViewModel::class.java]
+        categoryMealsViewModel = ViewModelProvider(this).get(CategoryMealsViewModel::class.java)
+
+        categoryMealsViewModel.getMealsByCategory(intent.getStringExtra(HomeFragment.CATEGORY_NAME)!!)
+
+        categoryMealsViewModel.observeMealsLiveData().observe(this, Observer { mealsList ->
+            binding.tvCategoryCount.text = mealsList.size.toString()
+            categoryMealsAdapter.setMealsList(mealsList)
+        })
+
+    }
+
+    private fun prepareRecyclerView() {
+        categoryMealsAdapter = CategoryMealsAdapter()
+        binding.rvMeals.apply {
+            layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
+            adapter = categoryMealsAdapter
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/foodapp/activities/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.foodapp.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.foodapp.R
+import com.example.foodapp.databinding.ActivityMainBinding
+import com.example.foodapp.db.MealDatabase
+import com.example.foodapp.videoModel.HomeViewModel
+import com.example.foodapp.videoModel.HomeViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+    val viewModel:HomeViewModel by lazy{
+        val mealDatabase = MealDatabase.getInstance(this)
+        val homeViewModelFactory = HomeViewModelFactory(mealDatabase)
+        ViewModelProvider(this, homeViewModelFactory)[HomeViewModel::class.java]
+    }
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val bottomNavigation = binding.btmNav
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
+
+    }
+}
